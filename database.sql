8ba8ab294609d654d299a102471248ac8bd88879
Fanto66 <65708700+Fanto66@users.noreply.github.com>
2025-02-27 12:55:10 +0100

    Finalisation du fichier db sql
    -Résolution des problèmes de syntaxe
    -Résolution des problèmes de données
    -Ajout d'une requête supplémentaire

--- database.sql
+++ database.sql
@@ -35,7 +35,7 @@ id_livraison INT PRIMARY KEY NOT NULL,
 date_livraison DATETIME NOT NULL,
 terminee BOOLEAN,
 retard BOOLEAN,
-station_metro: VARCHAR(50)
+station_metro VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS Transactions (
@@ -66,8 +66,8 @@ id_transaction INT,
 FOREIGN KEY (id_transaction) REFERENCES Transactions(id_transaction),
 id_livraison INT,
 FOREIGN KEY (id_livraison) REFERENCES Livraisons(id_livraison),
-nom_utilisteur_client_avis VARCHAR(50),
-FOREIGN KEY (nom_utilisateur_client_avis) REFERENCES Clients(nom_utilisateur_client)
+nom_utilisateur_client_avis VARCHAR(50),
+FOREIGN KEY (nom_utilisateur_client_avis) REFERENCES Clients(nom_utilisateur_client),
 titre_avis VARCHAR(50),
 note_avis INT CHECK (note_avis BETWEEN 0 AND 5),
 description_avis TEXT
@@ -90,7 +90,7 @@ FOREIGN KEY(id_ingredient) REFERENCES Ingredients(id_ingredient)
 CREATE TABLE IF NOT EXISTS Transac_Engendre_Livraison (
 id_transaction INT NOT NULL,
 id_livraison INT NOT NULL,
-PRIMARY KEY (id_transaction, id_plat),
+PRIMARY KEY (id_transaction, id_livraison),
 FOREIGN KEY (id_transaction) REFERENCES Transactions(id_transaction),
 FOREIGN KEY (id_livraison) REFERENCES Livraisons(id_livraison)
 );
@@ -98,11 +98,11 @@ FOREIGN KEY (id_livraison) REFERENCES Livraisons(id_livraison)
 
 -- Requetes d'insertion
 
---Clients
+-- Clients
 INSERT INTO Utilisateurs (nom_utilisateur, mot_de_passe, nom, prenom, rue, numero, code_postal, ville, telephone, email, metro_plus_proche)
 VALUES ("durand_mehdy17", "akJF&@-a98", "Durand", "Mehdi", "Rue Cardinet", 15, 75017, "Paris", "1234567890", "Mdurand@gmail.com", "Cardinet");
 INSERT INTO Clients(nom_utilisateur_client, nom_utilisateur)
-VALUES ("durant_mehdy17", "durant_mehdy17");
+VALUES ("durand_mehdy17", "durand_mehdy17");
 
 INSERT INTO Utilisateurs (nom_utilisateur, mot_de_passe, nom, prenom, rue, numero, code_postal, ville, telephone, email, metro_plus_proche)
 VALUES ("leclerc_marine89", "p@ssW0rd#12", "Leclerc", "Marine", "Avenue de la République", 42, 75011, "Paris", "0678123456", "marine.leclerc@gmail.com", "Republique");
@@ -119,7 +119,7 @@ VALUES ("martin_thomas30", "Pass*1234", "Martin", "Thomas", "Rue Lafayette", 78,
 INSERT INTO Clients(nom_utilisateur_client, nom_utilisateur)
 VALUES ("martin_thomas30", "martin_thomas30");
 
---Cuisiniers
+-- Cuisiniers
 INSERT INTO Utilisateurs (nom_utilisateur, mot_de_passe, nom, prenom, rue, numero, code_postal, ville, telephone, email, metro_plus_proche)
 VALUES ("marie.dupond", "aslkgjaop", "Dupond", "Marie", "Rue de la Republique", 30, 75011, "Paris", "1234567890", "Mdupond@gmail.com", "Republique");
 INSERT INTO Cuisiniers(nom_utilisateur_cuisinier, nom_utilisateur)
@@ -134,10 +134,12 @@ INSERT INTO Utilisateurs (nom_utilisateur, mot_de_passe, nom, prenom, rue, numer
 VALUES ("pablo.laxago", "motdepassechokbar", "Pablo", "Laxago", "Avenue Léonard de Vinci", 12, 92400, "Courbevoie", "0612344678", "pablo.laxago@gmail.com", "La Defense Grande Arche");  
 INSERT INTO Cuisiniers(nom_utilisateur_cuisinier, nom_utilisateur)  
 VALUES ("pablo.laxago", "pablo.laxago");
+INSERT INTO Clients(nom_utilisateur_client, nom_utilisateur)
+VALUES ("pablo.laxago", "pablo.laxago");
 
---Ingredients
+-- Ingredients
 INSERT INTO Ingredients
-VALUES (1, "Tomate")
+VALUES (1, "Tomate");
 INSERT INTO Ingredients
 VALUES (2, "Champignon");
 INSERT INTO Ingredients
@@ -161,7 +163,7 @@ VALUES (11, "Gambas");
 INSERT INTO Ingredients
 VALUES (12, "Poulet");
 
---Plats
+-- Plats
 INSERT INTO Plats (id_plat, nom, prix, quantite, type_plat, date_fabrication, date_peremption, regime, nationalite, disponible, nom_utilisateur_cuisinier)
 VALUES (1, "Pizza", 15.50, 4, "Plat congelé", "20250224", "20250315", "Omnivore", "Italienne", TRUE, "marie.dupond");
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
@@ -192,72 +194,87 @@ INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
 VALUES (3, 4, 175);
 
 INSERT INTO Plats (id_plat, nom, prix, quantite, type_plat, date_fabrication, date_peremption, regime, nationalite, disponible, nom_utilisateur_cuisinier)
-VALUES (4, "Paella", 28.75, 3, "Plat chaud", "20250227", "20250228", "Omnivore", "Espagnol", TRUE, "pablo.laxago")
+VALUES (4, "Paella", 28.75, 3, "Plat chaud", "20250227", "20250228", "Omnivore", "Espagnol", TRUE, "pablo.laxago");
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 7, 225); -- riz
+VALUES (4, 7, 225); -- riz
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 8, 80); -- rouge
+VALUES (4, 8, 80); -- rouge
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 9, 80); -- vert
+VALUES (4, 9, 80); -- vert
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 10, 10); -- ail
+VALUES (4, 10, 10); -- ail
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 11, 300); -- gambas
+VALUES (4, 11, 300); -- gambas
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 1, 150); -- tomate
+VALUES (4, 1, 150); -- tomate
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 1, 50); -- oignon
+VALUES (4, 5, 50); -- oignon
 INSERT INTO Composition_Plats (id_plat, id_ingredient, quantite)
-VALUES (3, 12, 450); -- poulet
+VALUES (4, 12, 450); -- poulet
 
---Transactions & livraisons
---Commande de 1 Lasagne
+-- Transactions & livraisons
+-- Commande de 1 Lasagne
 INSERT INTO Transactions (id_transaction, date_transaction, nom_utilisateur_client)
-VALUES (1, "20250226 7:14:07 PM", "durand_mehdy17");
+VALUES (1, "2025-02-26 19:14:07", "durand_mehdy17");
 
-UPDATE Plats SET (id_transaction = 1, disponible = FALSE) WHERE id_plat = 2;
+UPDATE Plats SET id_transaction = 1, disponible = FALSE WHERE id_plat = 2;
 
 INSERT INTO Livraisons (id_livraison, date_livraison, terminee, station_metro)
-VALUES (1, "20250226 7:15:55", FALSE, "Cardinet");
-INSERT INTO Engendre (id_transaction, id_livraison)
+VALUES (1, "2025-02-26 19:15:55", FALSE, "Cardinet");
+INSERT INTO Transac_Engendre_Livraison (id_transaction, id_livraison)
 VALUES (1, 1);
 
 UPDATE Plats SET id_livraison = 1 WHERE id_plat = 2;
 
---Commande de 1 Pizza et 1 Bolognaise à une autre adresse
+-- Commande de 1 Pizza et 1 Bolognaise à une autre adresse
 INSERT INTO Transactions (id_transaction, date_transaction, nom_utilisateur_client)
-VALUES (2, "20250227 5:38:43 PM", "leclerc_marine89");
+VALUES (2, "2025-02-27 17:38:43", "leclerc_marine89");
 
-UPDATE Plats SET (id_transaction = 2, disponible = FALSE) WHERE id_plat = 1;
-UPDATE Plats SET (id_transaction = 2, disponible = FALSE) WHERE id_plat = 3;
+UPDATE Plats SET id_transaction = 2, disponible = FALSE WHERE id_plat = 1;
+UPDATE Plats SET id_transaction = 2, disponible = FALSE WHERE id_plat = 3;
 
 INSERT INTO Livraisons (id_livraison, date_livraison, terminee, station_metro)
-VALUES (2, "20250227 5:45:03 PM", FALSE, "Republique")
-INSERT INTO Engendre (id_transaction, id_livraison)
+VALUES (2, "2025-02-27 17:45:03", FALSE, "Republique");
+INSERT INTO Transac_Engendre_Livraison (id_transaction, id_livraison)
 VALUES (2, 2);
 
 UPDATE Plats SET id_livraison = 2 WHERE id_plat = 1;
 
 INSERT INTO Livraisons (id_livraison, date_livraison, terminee, station_metro)
-VALUES (3, "20250227 5:48:15 PM", FALSE, "Franklin D. Roosevelt")
-INSERT INTO Engendre (id_transaction, id_livraison)
+VALUES (3, "2025-02-27 17:48:15", FALSE, "Franklin D. Roosevelt");
+INSERT INTO Transac_Engendre_Livraison (id_transaction, id_livraison)
 VALUES (2, 3);
 
 UPDATE Plats SET id_livraison = 3 WHERE id_plat = 3;
 
 -- Requetes de select
 
---Selectionner les livraisons en retard
+-- Selectionner les livraisons en retard
 SELECT * FROM Livraisons WHERE retard = TRUE;
 
---Selectionner les plats encore disponible
+-- Selectionner les plats encore disponible
 SELECT * FROM Plats WHERE disponible = TRUE;
 
---Selectionner l'adresse d'un utilisateur en particulier
+-- Selectionner l'adresse d'un utilisateur en particulier
 SELECT rue, numero, code_postal, ville
 FROM Utilisateurs
 WHERE nom_utilisateur = "martin_thomas30";
 
---Selectionner l'image du plat du jour d'un cuisinier
+-- Selectionner l'image du plat du jour d'un cuisinier
 SELECT photo_url FROM Plats
 WHERE plat_du_jour = TRUE AND nom_utilisateur_cuisinier = "jean.lemaitre";
+
+-- Selectionner les utilisateurs qui sont a la fois cuisiniers et clients
+SELECT DISTINCT nom_utilisateur_cuisinier
+FROM Cuisiniers
+WHERE nom_utilisateur_cuisinier IN (
+SELECT nom_utilisateur_client
+FROM Clients
+);
+
+-- Selection des plats qui sont en cours de livraison
+SELECT * FROM Plats
+WHERE id_livraison IN (
+SELECT id_livraison
+FROM Livraisons
+);
